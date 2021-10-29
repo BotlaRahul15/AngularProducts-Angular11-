2f797c6f913911ab312672617f88f4108bb88e22
BotlaRahul15 <91177870+BotlaRahul15@users.noreply.github.com>
2021-10-29 13:05:21 +0530

    Add files via upload

---
+++ src/app/product-details/product-details-form/product-details-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/shared/product-detail.model';
+import { NgForm } from '@angular/forms';
+import { ProductDetailService } from '../../shared/product-detail.service';
+
+@Component({
+  selector: 'app-product-details-form',
+  templateUrl: './product-details-form.component.html',
+  styleUrls: ['./product-details-form.component.css']
+})
+export class ProductDetailsFormComponent implements OnInit {
+
+  constructor(public service: ProductDetailService) { }
+
+  ngOnInit(): void {
+  }
+
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new Product();
+  }
+  onSubmit(form: NgForm) {
+    if (this.service.formData.productID == 0)
+      this.insertRecord(form);
+    else
+      this.updateRecord(form);
+  }
+
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshlist();
+      },
+      err => { console.log(err); }
+    )
+  }
+  updateRecord(form: NgForm) {
+    this.service.putPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshlist();
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+}
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Product } from '../shared/product-detail.model';
+import { ProductDetailService } from '../shared/product-detail.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor(public service: ProductDetailService) { }
+  ngOnInit() {
+    this.service.refreshlist();
+  }
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new Product();
+  }
+  populateForm(selectedRecord: Product) {
+    this.service.formData = Object.assign({}, selectedRecord);
+  }
+  onDelete(id: number) {
+    if (confirm('are you sure to delete this record ?')) {
+      debugger;
+      this.service.deletePaymentDetail(id)
+        .subscribe(res => {
+          this.service.refreshlist();
+        },
+          err => { console.log(err); })
+    }
+  }
+
+}
